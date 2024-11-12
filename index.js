8297bb893d415830aadd3b2ac1b06372576adc25
Oguzhan <oguzhanbesiktepe@gmail.com>
2024-11-12 10:04:51 -0500

    Linked ID's to each button in HTML to have JS, open a new tab with my socials when the specific button is clicked.

---
+++ index.js
@@ -0,0 +1,15 @@
+let github = document.getElementById("githubButton")
+github.addEventListener("click", function(){
+    window.open("https://github.com/OguzhanBesiktepe")
+})
+
+let instagram = document.getElementById("instagramButton")
+instagram.addEventListener("click", function(){
+    window.open("https://www.instagram.com/obby_besiktepe/")
+})
+
+let linkedIn = document.getElementById("linkedInButton")
+linkedIn.addEventListener("click", function(){
+    window.open("https://www.linkedin.com/in/oguzhan-besiktepe/")
+})
+
